7308a875df72d79061b7fc8b3b3b0e9ecae61c30
Viktor <git@vgsoftware.com>
2024-07-22 09:07:07 +0200

    Reworked project setup and added tool to test if roms start for compability.

--- Nescafe/Program.cs
+++ Nescafe/Program.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Nescafe.UI;
-using System.Diagnostics;
+﻿using Nescafe.UI;
 
 namespace Nescafe;
 
@@ -9,6 +7,39 @@ public class Program
 	[STAThread]
 	private static void Main(string[] args)
 	{
-		Application.Run(new Launcher());
+		Application.Run(new Launcher(args));
+
+
+		//var files = Directory.GetFiles(@"D:\Projects\nescafe\Roms", "*.nes");
+		//foreach (var file in files)
+		//{
+		//	try
+		//	{
+		//		args = new string[] { file };
+		//		var timer = new System.Timers.Timer();
+		//		var l = new Launcher(args);
+		//		timer.AutoReset = false;
+		//		timer.Interval = 1000;
+		//		timer.Elapsed += (sender, e) =>
+		//		{
+		//			try
+		//			{
+		//				l.Exit();
+		//			}
+		//			catch
+		//			{
+		//			}
+		//		};
+		//		timer.Start();
+		//		Application.Run(l);
+		//		File.Move(file, Path.Combine("D:\\Projects\\nescafe\\Roms\\working", Path.GetFileName(file)));
+		//	}
+		//	catch(Exception ex)
+		//	{
+		//		Console.WriteLine("Error:" + ex.ToString());
+		//		File.Move(file, Path.Combine("D:\\Projects\\nescafe\\Roms\\Not working", Path.GetFileName(file)));
+		//	}
+		//}
+		//Console.WriteLine("Exit");
 	}
 }
---
+++ tools/Test Compability/Program.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Nescafe.Test_Compability;
+
+public class Program
+{
+	private static Thread _nesThread;
+	private static Thread _killThread;
+	private static Core.Console _console;
+	private static bool _isRunning = false;
+	private static string _currentFile;
+	private static List<string> _workingFiles = new List<string>();
+	private static List<string> _notWorkingFiles = new List<string>();
+
+	private static void TestRom(string path)
+	{
+		_currentFile = path;
+		_console = new Core.Console();
+		_isRunning = true;
+		if (LoadROM(path))
+		{
+			while (_isRunning)
+			{
+				Thread.Sleep(500);
+			}
+		}
+	}
+
+	private static bool LoadROM(string path)
+	{
+		try
+		{
+			if (File.Exists(path))
+			{
+				if (_console.LoadCartridge(path))
+				{
+					StartConsole();
+					return true;
+				}
+				else
+				{
+					_notWorkingFiles.Add(_currentFile);
+					Console.WriteLine($"Not loading: {path}");
+				}
+			}
+		}
+		catch
+		{
+			_notWorkingFiles.Add(_currentFile);
+			Console.WriteLine($"Not working");
+		}
+		return false;
+	}
+
+	private static void StartConsole()
+	{
+		_nesThread = new Thread(new ThreadStart(StartNes));
+		_nesThread.IsBackground = true;
+		_nesThread.Start();
+
+		_killThread = new Thread(new ThreadStart(StopConsole));
+		_killThread.IsBackground = true;
+		_killThread.Start();
+	}
+
+	private static void StopConsole()
+	{
+		Thread.Sleep(1500);
+		_console.Stop();
+		_isRunning = false;
+		Console.WriteLine("Seems to be working");
+		_workingFiles.Add(_currentFile);
+	}
+
+	private static void StartNes()
+	{
+		try
+		{
+			_console.Start();
+		}
+		catch
+		{
+			_notWorkingFiles.Add(_currentFile);
+			Console.WriteLine($"Not working");
+		}
+		_isRunning = false;
+	}
+
+	static void Main(string[] args)
+	{
+		var files = Directory.GetFiles(@"D:\Projects\nescafe\Roms", "*.nes");
+		foreach (var file in files)
+		{
+			TestRom(file);
+		}
+		_workingFiles.Insert(0, $"Working: {_workingFiles.Distinct().Count()}");
+		File.WriteAllLines(@$"D:\Projects\nescafe\logs\working_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}.txt", _workingFiles.Distinct());
+		_notWorkingFiles.Insert(0, $"Working: {_notWorkingFiles.Distinct().Count()}");
+		File.WriteAllLines(@$"D:\Projects\nescafe\logs\not_working_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}.txt", _notWorkingFiles.Distinct());
+	}
+}
